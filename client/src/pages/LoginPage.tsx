e4c6fc277521813655f7c530e5c1771ea922ee08
HsrRaza <hr.raza04@gmail.com>
2026-03-05 15:34:46 +0530

    added login page

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,8 +1,60 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { cn } from "../lib/utils"
+import { Card , CardHeader , CardTitle , CardDescription , CardContent } from "../components/ui/card"
+import { Button } from "../components/ui/button"
+import { Input } from "../components/ui/input"
+import { Field , FieldGroup , FieldLabel , FieldDescription } from "../components/ui/field"
 
-
-const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
+const LoginPage = ({className, ...props}: any) => {
+  return  (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+     <section className="flex justify-center items-center h-screen">
+       <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit">Login</Button>
+                <Button variant="outline" type="button">
+                  Login with Google
+                </Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <a href="#">Sign up</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+     </section>
+    </div>
   )
 }
 
